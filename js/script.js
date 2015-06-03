169285c779985912ec833cdce833d0da18c64d30
Jessica Engel & Jennifer Bolanos <student@epicodus.com>
2015-06-03 09:30:45 -0700

    initialize Player and Space function

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function Player(mark) {
+  this.mark = mark
+};
+
+function Space(xcoordinate) {
+  this.xcoordinate = xcoordinate
+};
+
+//jQuery
+// $( document ).ready(function() {
+//
+// }
---
+++ spec/specs.js
@@ -0,0 +1,13 @@
+describe('Player', function() {
+  it("returns the player's mark", function() {
+    var testPlayer = new Player("X");
+    expect(testPlayer.mark).to.equal("X");
+  });
+});
+
+describe('Space', function() {
+  it("returns the player's mark", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.xcoordinate).to.equal(1);
+  });
+});
