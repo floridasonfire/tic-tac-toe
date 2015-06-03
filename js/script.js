c378137784ac26cdb904b19e492662dd12ca9684
Jessica Engel & Jennifer Bolanos <student@epicodus.com>
2015-06-03 10:17:56 -0700

    creates a board

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
 "use strict";
 
 function Player(mark) {
-  this.mark = mark
+  this.mark = mark;
 };
 
-function Space(xcoordinate) {
-  this.xcoordinate = xcoordinate
+function Space(xcoordinate, ycoordinate) {
+  this.xcoordinate = xcoordinate;
+  this.ycoordinate = ycoordinate;
+  var markBy = " ";
+  this.markBy = markBy
 };
+
+Space.prototype.mark_by = function(markingPlayer){
+  this.markBy = markingPlayer;
+  console.log(this.markBy)
+
+};
+
+function Board() {
+  var boardArray = []
+  this.boardArray = boardArray
+
+  boardArray.push(new Space(1,1));
+  boardArray.push(new Space(2,1));
+  boardArray.push(new Space(3,1));
+  boardArray.push(new Space(1,2));
+  boardArray.push(new Space(1,3));
+  boardArray.push(new Space(2,2));
+  boardArray.push(new Space(3,2));
+  boardArray.push(new Space(2,3));
+  boardArray.push(new Space(3,3));
+}
 
 //jQuery
 // $( document ).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -10,4 +10,23 @@ describe('Space', function() {
     var testSpace = new Space(1,2);
     expect(testSpace.xcoordinate).to.equal(1);
   });
+
+  it("returns the player's mark", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.ycoordinate).to.equal(2);
+  });
+
+  it("lets a player mark a space", function() {
+      var testPlayer = new Player("X")
+      var testSpace = new Space(1,2);
+      testSpace.mark_by(testPlayer)
+      expect(testSpace.markBy).to.equal(testPlayer);
+   });
 });
+
+describe('Board', function() {
+  it("creates 9 spaces when it is initialized", function() {
+    var testBoard = new Board()
+    expect(testBoard.boardArray.length).to.equal(9)
+  })
+})
